1c590ddebdeb283d02ce4254031b01186dc98d8c
MalkamGolden <45684143+MalkamGolden@users.noreply.github.com>
2019-10-08 05:26:49 -0700

    Update ByteCodeLoader.java

--- interpreter/ByteCodeLoader.java
+++ interpreter/ByteCodeLoader.java
@@ -4,30 +4,79 @@ package interpreter;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+import interpreter.bytecode.*;
 
 
 public class ByteCodeLoader extends Object {
 
+    // reads input
     private BufferedReader byteSource;
-    
+    // program object to run in VM
+    private Program program;
+
     /**
-     * Constructor Simply creates a buffered reader.
-     * YOU ARE NOT ALLOWED TO READ FILE CONTENTS HERE
-     * THIS NEEDS TO HAPPEN IN LOADCODES.
+     * Constructor for ByteCodeLoader
+     *
+     * @param file input file
+     * @throws IOException
      */
     public ByteCodeLoader(String file) throws IOException {
         this.byteSource = new BufferedReader(new FileReader(file));
     }
+
     /**
-     * This function should read one line of source code at a time.
-     * For each line it should:
-     *      Tokenize string to break it into parts.
-     *      Grab THE correct class name for the given ByteCode from CodeTable
-     *      Create an instance of the ByteCode class name returned from code table.
-     *      Parse any additional arguments for the given ByteCode and send them to
-     *      the newly created ByteCode instance via the init function.
+     * Constructs a program with the input bytecode objects
+     *
+     * @return the program
      */
     public Program loadCodes() {
-       return null;
+
+        try {
+
+            program = new Program();
+
+            // reads each line from input
+            String line = byteSource.readLine();
+            while (line != null) {
+
+                ArrayList<String> args = new ArrayList<>();
+                //tokenize input
+                StringTokenizer tokenizer = new StringTokenizer(line);
+                String code = tokenizer.nextToken();
+
+                // if code exists
+                if (!code.isEmpty()) {
+                    //get class name of the code to fetch
+                    String codeClass = CodeTable.getClassName(code);
+
+                    // add arguments
+                    while (tokenizer.hasMoreTokens()) {
+                        args.add(tokenizer.nextToken());
+                    }
+
+                    //makes the bytecode object
+                    ByteCode byteCode = (ByteCode) Class.forName("interpreter.ByteCode." + codeClass).newInstance();
+                    byteCode.init(args);
+
+                    // adds bytecode to program
+                    program.add(byteCode);
+
+                    // gets next line
+                    line = byteSource.readLine();
+                }
+            }
+
+            // resolves addresses for labels and jumps
+            program.resolveAddrs(program);
+
+        } catch (InstantiationException | ClassNotFoundException | IllegalAccessException | IOException e) {
+            System.out.println(e);
+        }
+
+        // return constructed program
+        return program;
     }
+
 }
